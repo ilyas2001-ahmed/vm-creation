5303382abb389c9787965e2bc3fd66ab195809c5
ilyas2001-ahmed <ilyas2001@gmail.com>
2025-01-30 16:58:33 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,30 @@
 
+provider "azurerm" {
+  subscription_id   = "d5e7dbaf-2eb0-4923-9aab-6804988d16fd"
+  client_id       = "20f4d646-110b-465e-ab61-f06b4c9d0657"
+  client_secret   = "H8t8Q~O3MXXSVMzu6i_UYGeRDpGEjMuj-1NbibJ~"
+  tenant_id       = "c6054cb1-821f-4197-b046-72ee2feb2f50"
+  features        {}
+}
+
+
+/*
+resource "azurerm_resource_group" "hr" {
+  name     = "RG-PROJECT02-HR"
+  location = "East US"
+
+}
+
+
+resource "azurerm_resource_group" "sales" {
+  name     = "RG-PROJECT03-Sales"
+  location = "East US"
+
+}
+*/
+
+
+
+
+
+
